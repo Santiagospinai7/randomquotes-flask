7c0a2ee5fb2152a5e7678b06d3475d17b11ee4b3
Santiago Ospina Idrobo <51349626+Santiagospinai7@users.noreply.github.com>
2023-10-31 08:00:26 -0500

    Update randomquotes.py

--- randomquotes.py
+++ randomquotes.py
@@ -1,26 +1,16 @@
 from flask import Flask
-import os
-import random
 
 app = Flask(__name__)
 
-phrases = [
-    "Get ready to be inspired…", 
-    "See rejection as redirection.",
-    "There is beauty in simplicity.",
-    "You can’t be late until you show up.",
-    "Maybe life is testing you. Don’t give up.",
-    "Impossible is just an opinion.",
-    "Alone or not you gonna walk forward.",
-]
-
-
-@app.route('/')
-def get_random_quote():
-    phrase = random.choice(phrases)
-    container_id = os.uname()[1] 
-    return f"{phrase} - Container Id: {container_id}"
+@app.route('/factorial/<int:num>')
+def factorial(num):
+  if num < 0:
+    return "Error: No se puede calcular el factorial de un número negativo."
+  
+  result = 1
+  for i in range(1, num+1):
+    result *= i
+  return str(result)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
-
+  app.run(host='0.0.0.0', port=3003)
